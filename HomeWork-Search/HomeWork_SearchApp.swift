09901785fb88c939efdeb023a8af2b858e6fd129
官洋 <yangguan@isoftstone.com>
2021-09-30 19:47:27 +0800

    Initial Commit

---
+++ HomeWork-Search/HomeWork_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_SearchApp.swift
+//  HomeWork-Search
+//
+//  Created by Michel on 2021/9/30.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
